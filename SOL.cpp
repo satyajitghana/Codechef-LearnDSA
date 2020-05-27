8889f5b16869380c395674f7ab4faf6b3a5e7d6b
Satyajit Ghana <satyajitghana7@gmail.com>
2020-05-28 05:04:30 +0530

    add template

---
+++ SOL.cpp
@@ -0,0 +1,22 @@
+// template
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define lld long long int
+
+static void print_arr(int arr[], int N) {
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << ' ';
+    }
+
+    cout << '\n';
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+    }
+    return 0;
+}
