71c7708e9656a9a599a2e0ec0393941497169d83
Satyajit Ghana <satyajitghana7@gmail.com>
2020-06-07 14:06:20 +0530

    update

---
+++ TEST/SOL.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int A[5] = {1, 2, 3, 4, 5};
+
+    cout << A[10000] << ' ';
+
+    return 0;
+}
